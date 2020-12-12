c324fd500276d5379b1a95e17b712c9e816032b9
Sergio Gasquez Arcos <sergio.gasquez@gmail.com>
2020-12-12 14:42:37 +0100

    Clean Dockerfile. Update dependencies version

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -18,10 +18,10 @@ macro(run_conan)
   conan_cmake_run(
     REQUIRES
     ${CONAN_EXTRA_REQUIRES}
-    catch2/2.11.0
-    docopt.cpp/0.6.2
-    fmt/6.2.0
-    spdlog/1.5.0
+    catch2/2.13.3
+    docopt.cpp/0.6.3
+    fmt/7.1.2
+    spdlog/1.8.1
     OPTIONS
     ${CONAN_EXTRA_OPTIONS}
     BASIC_SETUP
