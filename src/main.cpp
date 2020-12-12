5f02c577c59195e949651340a34fe81664dcb565
Sergio Gasquez Arcos <sergio.gasquez@gmail.com>
2020-12-12 16:36:11 +0100

    Change main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 int main()
 {
   //Use the default logger (stdout, multi-threaded, colored)
-  std::vector<int> test = { 2, 2, 2, 2 };
-  int arr[] = { 2, 2, 2, 2, 2 };
-  for (int i = 0; i <= 3; i++)
-  {
-    fmt::print("{}\n", int(85));
-  }
+  // std::vector<int> test = { 2, 2, 2, 2 };
+  // int arr[] = { 2, 2, 2, 2, 2 };
+  // for (int i = 0; i <= 3; i++)
+  // {
+  //   fmt::print("{}\n", int(85));
+  // }
   spdlog::info("Hello, {}!", "World");
 
   fmt::print("Hello, from {}\n", "{fmt}");
